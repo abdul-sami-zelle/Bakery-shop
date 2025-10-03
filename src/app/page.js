e434b8d4eb7aa7e0f05cff299ac9bc2479d86a30
abdul-sami-zelle <abdulsami.zellesolutions@gmail.com>
2025-10-03 18:59:31 +0500

    second

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,14 @@
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import styles from "./page.module.css";
-import Header from "./components/Header/header";
+// import Header from "./components/Header/header";
 import Hero from "./components/HeroSection/herosection";
 import Categories from "./components/categories/Categories";
 import ProductSlider from "./components/products/products";
 import { getLandingPageData } from "./lib/api";
 import SalesSection from "./components/saleSection/salesSection";
+import MainSlider from "./components/mainSlider/mainSlider";
+import Header from "./utils/Header/Header";
 
 export default function Home() {
   const [categories, setCategories] = useState([]);
@@ -28,7 +30,7 @@ export default function Home() {
   return (
     <div >
       <Header />
-      <Hero />
+      <MainSlider/>
       <SalesSection/>
       <div className="main_sections">
 
